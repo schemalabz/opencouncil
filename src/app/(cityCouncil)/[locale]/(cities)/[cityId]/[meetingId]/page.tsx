6171127de70352e58afeb0bfa46e3ff4dfd67822
Christos Porios <christos.porios@gmail.com>
2024-12-11 12:57:53 +0200

    mapbox map

--- src/app/(cityCouncil)/[locale]/(cities)/[cityId]/[meetingId]/page.tsx
+++ src/app/(cityCouncil)/[locale]/(cities)/[cityId]/[meetingId]/page.tsx
@@ -1,3 +1,52 @@
+"use client"
+import Map from "@/components/map/map";
+import { useCouncilMeetingData } from "@/components/meetings/CouncilMeetingDataContext";
+import { format, formatDate } from "date-fns";
+import { CalculatorIcon, CalendarIcon, FileIcon, VideoIcon } from "lucide-react";
+
 export default function MeetingPage() {
-    return <div>MeetingPage</div>;
+    const { meeting, subjects } = useCouncilMeetingData();
+    return (
+        <div className="flex flex-col w-full">
+            <div className="relative h-[300px] w-full">
+                <Map className="w-full h-full" />
+                <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white to-transparent" />
+                <MeetingInfo />
+            </div>
+        </div>
+    )
+}
+
+function MeetingInfo() {
+    const { meeting, subjects } = useCouncilMeetingData();
+    return (
+        <div className="absolute bottom-0 left-0 right-0 p-6">
+            <div className="max-w-4xl mx-auto">
+                <h1 className="text-2xl font-bold mb-2">{meeting.name}</h1>
+                <div className="flex flex-wrap gap-4 text-sm text-gray-600">
+                    <div className="flex items-center">
+                        <CalendarIcon className="w-4 h-4 mr-2" />
+                        {formatDate(new Date(meeting.dateTime), 'PPP')}
+                    </div>
+                    {meeting.videoUrl && (
+                        <div className="flex items-center">
+                            <VideoIcon className="w-4 h-4 mr-2" />
+                            {meeting.videoUrl ? "Βίντεο Διαθέσιμο" : meeting.audioUrl ? "Μόνο ήχος" : "Χωρίς βίντεο/ήχο"}
+                        </div>
+                    )}
+                    {subjects.length > 0 ? (
+                        <div className="flex items-center">
+                            <FileIcon className="w-4 h-4 mr-2" />
+                            {subjects.length} θέματα
+                        </div>
+                    ) : (
+                        <div className="flex items-center">
+                            <FileIcon className="w-4 h-4 mr-2" />
+
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
 }
