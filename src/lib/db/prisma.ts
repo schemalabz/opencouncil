3bd721675908cb4231301b20c776c62146160ba9
Christos Porios <christos.porios@gmail.com>
2025-01-23 09:59:38 +0200

    with accelerate

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaClient as EdgePrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
+    if (process.env.WITH_PRISMA_ACCELERATE) {
+        return new EdgePrismaClient().$extends(withAccelerate())
+    }
     return new PrismaClient({
         // log: ['query']
     })
