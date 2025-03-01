c31949ddb48758a7228d99c67ef6f35dc238af7a
Christos Porios <christos.porios@gmail.com>
2025-03-01 23:34:53 +0200

    Revert "tempi banner"
    This reverts commit b25a63d3cfe958cb9e822d9a49033c762aabacd4.

--- src/app/[locale]/(other)/layout.tsx
+++ src/app/[locale]/(other)/layout.tsx
@@ -3,14 +3,6 @@ import Header from "@/components/layout/Header"
 import Footer from "@/components/layout/Footer"
 import { Toaster } from "@/components/ui/toaster";
 
-const Banner = () => {
-  return (
-    <div className="bg-black text-white p-2">
-      <p className="text-center">To OpenCouncil συμμετέχει στις σημερινές κινητοποιήσεις για τα Τέμπη.</p>
-    </div>
-  )
-}
-
 export default async function Layout({
   children,
   params: { locale }
@@ -21,7 +13,6 @@ export default async function Layout({
 
   return (
     <>
-      <Banner />
       <Header path={[]} />
       <main className="min-h-[70vh] mt-[65px]">
         {children}
