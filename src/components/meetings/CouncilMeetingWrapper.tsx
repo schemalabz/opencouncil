1783ec116dcee45f6bb1f769371b577b58c9101c
kouloumos <kouloumosa@gmail.com>
2025-08-12 17:01:04 +0300

    refactor(meetings): remove CouncilMeeting component and streamline CouncilMeetingWrapper
    - Deleted the CouncilMeeting component to simplify the codebase and improve maintainability.
    - Updated CouncilMeetingWrapper to remove unused imports and unnecessary state management, enhancing performance and clarity.

--- src/components/meetings/CouncilMeetingWrapper.tsx
+++ src/components/meetings/CouncilMeetingWrapper.tsx
@@ -1,22 +1,15 @@
 "use client"
 
-import { useState, useEffect, useRef, useMemo, createContext, useContext } from 'react'
-import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet"
-import { Play, Pause, MessageSquare, FileText, CheckCircle, BotMessageSquare, NotepadText, Settings2, LayoutList, Sparkles, X, Wrench, Share, Loader, Menu, ChartArea, BarChart, BarChart2, BarChart3, MessageSquareQuote } from "lucide-react"
-import AdminActions from './admin/Admin'
-import { useVideo, VideoProvider } from './VideoProvider'
-import { motion, AnimatePresence } from 'framer-motion'
-import { Options } from './options/Options'
+import { useState, useEffect, useMemo, createContext, useContext } from 'react'
+import { Loader } from 'lucide-react'
+import { VideoProvider } from './VideoProvider'
+import { motion } from 'framer-motion'
 import { TranscriptOptionsProvider } from './options/OptionsContext'
 import { CouncilMeetingDataProvider } from './CouncilMeetingDataContext'
 import { HighlightProvider } from './HighlightContext'
-import { Statistics } from './Statistics'
-import ShareC from './Share'
-import Summary from './Summary'
-import Highlights from '../Highlights'
-import Subjects from './Subjects'
 import { MeetingData } from '@/lib/getMeetingData'
 import { KeyboardShortcuts } from './KeyboardShortcuts'
+
 
 type CouncilMeetingWrapperProps = {
     editable: boolean,
@@ -29,12 +22,7 @@ export const useLayout = () => useContext(LayoutContext);
 
 export default function CouncilMeetingWrapper({ meetingData, editable, children }: CouncilMeetingWrapperProps) {
     const [isWide, setIsWide] = useState(false);
-    const [activeSection, setActiveSection] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
-    const [mode, setMode] = useState<'transcript' | 'highlights'>('transcript');
-    const highlightId = typeof window !== 'undefined' && window.location.hash.startsWith('#h-')
-        ? window.location.hash.slice(3)
-        : null;
 
     const memoizedUtterances = useMemo(() => {
         return meetingData.transcript.map((u) => u.utterances).flat()
@@ -51,26 +39,8 @@ export default function CouncilMeetingWrapper({ meetingData, editable, children 
         window.addEventListener('resize', checkSize)
         setLoading(false)
 
-        // Check if URL hash starts with #h- and set mode to 'highlights' if it does
-        if (typeof window !== 'undefined' && window.location.hash.startsWith('#h-')) {
-            setMode('highlights');
-        }
-
         return () => window.removeEventListener('resize', checkSize)
     }, [])
-
-    const sections = [
-        { title: "Τοποθετήσεις", icon: <MessageSquareQuote />, content: <Summary /> },
-        { title: "Θέματα", icon: <LayoutList />, content: <Subjects /> },
-        { title: "Στατιστικά", icon: <BarChart3 />, content: <Statistics /> },
-        { title: "Κοινοποίηση", icon: <Share />, content: <ShareC /> },
-        { title: "Επιλογές", icon: <Settings2 />, content: <Options editable={editable} /> },
-    ]
-
-    if (editable) {
-        sections.push({ title: "Highlights", icon: <Sparkles />, content: <Highlights highlights={meetingData.highlights} /> })
-        sections.push({ title: "Admin", icon: <CheckCircle />, content: <AdminActions /> })
-    }
 
     if (loading) return (
         <motion.div
