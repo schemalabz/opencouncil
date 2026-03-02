aed8ec5f6615a2e660d79bad5878290f05ba66a8
NektariosTP <nektar.tp@gmail.com>
2026-03-02 22:25:10 +0200

    feat(about): pause carousel on hover and make city icons clickable
    The ΣΥΝΕΡΓΑΖΟΜΕΝΟΙ ΔΗΜΟΙ marquee carousel now:
    - Pauses scrolling when the user hovers over it (pauseOnHover=true)
    - Each city logo is wrapped in a Link navigating to /{city.id},
      with a title tooltip showing the municipality name and cursor-pointer

--- src/components/static/About.tsx
+++ src/components/static/About.tsx
@@ -230,12 +230,15 @@ export default function AboutPage({ citiesWithLogos }: AboutPageProps) {
                                 <Marquee
                                     className="[--duration:30s] [--gap:2rem] sm:[--gap:2.5rem] md:[--gap:3rem] p-0 justify-center"
                                     reverse={false}
-                                    pauseOnHover={false}
+                                    pauseOnHover={true}
                                     repeat={5}
                                 >
                                     {citiesWithLogos.map((city) => (
-                                        <div
+                                        <Link
                                             key={city.id}
+                                            href={`/${city.id}`}
+                                            title={city.name_municipality}
+                                            tabIndex={-1}
                                             className="flex-shrink-0 flex items-center justify-center w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 transition-opacity hover:opacity-80"
                                         >
                                             {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -244,7 +247,7 @@ export default function AboutPage({ citiesWithLogos }: AboutPageProps) {
                                                 alt={city.name_municipality}
                                                 className="w-full h-full object-contain"
                                             />
-                                        </div>
+                                        </Link>
                                     ))}
                                 </Marquee>
                             </div>
